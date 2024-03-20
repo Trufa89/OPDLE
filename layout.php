13a9298b380b6d5438318ec5b32e2bbc2682ceb3
Trufa89 <164177179+Trufa89@users.noreply.github.com>
2024-03-20 13:23:56 +0100

    Update layout.php

--- layout.php
+++ layout.php
@@ -7,7 +7,7 @@
     <!-- Enlace al archivo CSS de Bootstrap desde un CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Enlace al archivo CSS personalizado -->
-    <link rel="stylesheet" href="../vista/css/styles.css">
+    <link rel="stylesheet" href="styles.css">
     <!-- Enlace al archivo JS de Bootstrap desde un CDN -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <!-- Enlace a jQuery desde un CDN -->
